eb92ceff384c0aaf841e8de2ca7ecdfd9532abb0
rsarge47 <35145102+rsarge47@users.noreply.github.com>
2018-04-29 13:57:46 -0400

    Added bodyparser, 2 controller files, htmlController is for html routes and vendorController is for api routes.  The routes return JSON data from the DB.

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -3,12 +3,26 @@ const router = express.Router();
 
 import db from '../models';
 
-// import db model 
+router.get('/api/vendors', (req, res) => {
+    db.Vendor.findAll({}).then(result => res.json(result));
+});
 
-router.get('/', (req, res) => {
-    // pass in home page with 
-    res.render('index')
-    //res.send('Hello Roe4');
+router.get('/api/vendors/:id', (req, res) => {
+    db.Vendor.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(result => res.json(result));
+});
+
+router.get('/api/city/:city', (req, res) => {
+    db.Vendor.findAll({
+        where: {
+            city: req.params.city
+        }
+    })
+    .then(result => res.json(result));
 });
 
 // router.post('/api/vendors', (req, res) => {
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import db from './models';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 import exphbs from 'express-handlebars';
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-import router from './controllers/vendorController.js';
+import pages from './controllers/htmlController.js';
+import vendors from './controllers/vendorController.js';
 
-app.use(router);
+app.use(pages);
+app.use(vendors);
 
 db.sequelize.sync().then( () => {
     app.listen(PORT, () => console.log('App now listening at localhost: ', PORT));

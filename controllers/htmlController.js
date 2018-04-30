c05972d45218adeb820faaf4d887f282cf65fe9d
MeliaNolen <mnolen5670@gmail.com>
2018-04-30 17:39:44 -0400

    fixed navbar, have not styled it

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -4,22 +4,22 @@ const router = express.Router();
 import db from '../models';
 
 router.get('/', (req, res) => {
-    // pass in home page with 
-    res.render('index')
+    res.render('index');
 });
 
 router.get('/buyer', (req, res) => {
-    // pass in home page with 
     res.render('buyerProfile');
 });
 
 router.get('/vendor', (req, res) => {
-    // pass in home page with 
     res.render('farmerProf');
 });
 
+router.get('/vendor/manageproducts', (req, res) => {
+    res.render('farmerEdit');
+});
+
 router.get('/buyer/search', (req, res) => {
-    // pass in home page with 
     res.render('buyerSearch');
 });
 

24b35ae95599e5d9362aaefaa4e1c3d39cbf5f92
dtcrowley <crowleyd1988@gmail.com>
2018-05-07 14:49:58 -0400

    heroku test

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var mysql = require('mysql');
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
@@ -7,12 +8,19 @@ var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(path.join(__dirname + './../config/config.json'))[env];
 var db        = {};
+var connection;
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+  connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'vfm'
+  });
+};
 
 fs
   .readdirSync(__dirname)
